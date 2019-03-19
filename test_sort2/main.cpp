d56892577fc2bf35339b3ada10fc39f04ef5ef9a
roman-modelist-dev <ibr.roman@gmail.com>
2019-03-19 20:38:56 +0000

    sort success

--- test_sort2/main.cpp
+++ test_sort2/main.cpp
@@ -12,8 +12,6 @@
 int main() {
   //std::vector<std::string> in_data = {"asdfqw1234325", "af3t t9unj9-wn=0vmerwg", "12213", "dfjhgkr", "mhioertphnj SGSdgadg"};
   
-  std::random_device rd;
-  std::uniform_int_distribution<uint64_t> dist(0, (uint32_t)-1);
   
   /*std::vector<int> in_data1(0x0fffffff);
   for(int i = 0; i < in_data1.size(); ++i)
@@ -21,37 +19,74 @@ int main() {
     in_data1[i] = dist(rd);
   }*/
   
- // std::vector<int> in_data1(0x0fffffff);
-  std::vector<int> in_data1(0x000fffff);
+//std::vector<int> in_data1(0x0fffffff);
+  std::vector<int> in_data1(0x08000000);
+  std::vector<int> in_data2(0x08000000);
+  std::vector<int> in_data3(0x08000000);
+  
+  auto block_size = in_data1.size()/8;
+  size_t current_ind = 0;
+  std::vector<std::future<void>> results;
+  std::random_device rd;
+  std::uniform_int_distribution<uint64_t> dist(0, (uint32_t)-1);
+  
   for(int i = 0; i < in_data1.size(); ++i)
   {
-    in_data1[i] = dist(rd);
-  }
-  /*
-  std::vector<int> in_data1 = {
-   5, 60, 23, 6, 14, 39, 13, 1, 49, 6, 7, 4, 10, 8, 5, 8, 9, 3, 5, 23, 5, 60, 23, 6, 14, 39, 13, 1, 49, 6, 7, 4, 20, 8,
-    5, 8, 9, 3, 5, 23, 5, 60, 23, 6, 14, 39, 13, 1, 49, 6, 7, 4, 10, 8, 5, 8, 9, 3, 5, 23, 5, 60, 23, 6, 14, 39, 13, 1,
-   5, 60, 23, 6, 14, 39, 13, 1, 49, 6, 7, 4, 10, 8, 5, 8, 9, 3, 5, 23, 5, 60, 23, 6, 14, 39, 13, 1, 49, 6, 7, 4, 20, 8,
-   5, 8, 9, 3, 5, 23, 5, 60, 23, 6, 14, 39, 13, 1, 49, 6, 7, 4, 10, 8, 5, 8, 9, 3, 5, 23, 5, 60, 23, 6, 14, 39, 13, 1,
-   49, 6, 7, 4, 20, 8,
-  };*/
-  
+      /*  static int x = 0;
+        ++x;
+        if(x%4 == 0)
+          tmp = dist(rd);*/
+        in_data1[i] = dist(rd);
+   }
+   
+  std::copy(in_data1.begin(), in_data1.end(), in_data2.begin());
+  std::copy(in_data1.begin(), in_data1.end(), in_data3.begin());
+ /*
+  std::vector<int> in_data2 = {
+   1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32
+  };
+ */
  // auto& thread_pool = thread_pool_t::instance();
   
-  auto start = std::chrono::system_clock::now();
-  parallel_merge_sort(in_data1.begin(), in_data1.end());
-  //std::sort(in_data1.begin(), in_data1.end());
-  //boost::sort::block_indirect_sort(in_data1.begin(), in_data1.end());
-  auto end = std::chrono::system_clock::now();
+  {
+    auto start = std::chrono::system_clock::now();
+    //parallel_merge_sort(in_data1.begin(), in_data1.end());
+    std::sort(in_data1.begin(), in_data1.end());
+    //boost::sort::block_indirect_sort(in_data1.begin(), in_data1.end());
+    auto end = std::chrono::system_clock::now();
   
-  std::chrono::milliseconds diff = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
-  std::cout << "Sort time = " << diff.count() << " ms\n";
+    std::chrono::milliseconds diff = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
+    std::cout << "STD Sort time = " << diff.count() << " ms\n";
+  }
+  
+  {
+    auto start = std::chrono::system_clock::now();
+    parallel_merge_sort(in_data2.begin(), in_data2.end());
+    //boost::sort::block_indirect_sort(in_data1.begin(), in_data1.end());
+    auto end = std::chrono::system_clock::now();
+    
+    std::chrono::milliseconds diff = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
+    std::cout << "Parallel Sort time = " << diff.count() << " ms\n";
+  }
+  {
+    auto start = std::chrono::system_clock::now();
+    boost::sort::block_indirect_sort(in_data3.begin(), in_data3.end());
+    auto end = std::chrono::system_clock::now();
+    
+    std::chrono::milliseconds diff = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
+    std::cout << "Boost Sort time = " << diff.count() << " ms\n";
+  }
+  
   //boost::sort::block_indirect_sort(in_data1.begin(), in_data1.end());
   //string_list in_data = {5,23,39,6,14,13,1,6,7,4,8,5,8,9,3,5,23};
   //string_list in_data = {};
   //string_list in_data = {5,23,39,6,14,13,1,6,7,4,8,5,8,9,3,5,23};
   //string_list in_data = {30,23,39};
   
-  int d = 0;
+  bool res = std::equal(in_data1.begin(), in_data1.end(), in_data2.begin());
+  if(res)
+    std::cout << "All OK" << std::endl;
+  else
+    std::cout << "FAIL" << std::endl;
   return 0;
 }
--- test_sort2/sort.hpp
+++ test_sort2/sort.hpp
@@ -10,6 +10,7 @@
 #include <algorithm>
 #include <type_traits>
 #include <cstddef>
+#include <boost/sort/common/range.hpp>
 
 #include "thread_pool.hpp"
 
@@ -42,12 +43,24 @@ void merge(Iterator_in first_begin, Iterator_in first_end, Iterator_in second_be
 
 size_t count_merges(size_t length);
 
+using namespace boost::sort::common;
+
 template <typename Iterator>
-void parallel_merge_sort(Iterator begin, Iterator last)
-{
+void parallel_merge_sort(Iterator begin, Iterator last) {
   using value_type = typename Iterator::value_type;
   using buffer_t = std::vector<value_type>;
-  using buffer_pair_t = std::pair<buffer_t&, buffer_t&>;
+  struct buffer_pair_t
+  {
+    buffer_pair_t(buffer_t* in, buffer_t* out):in_buf(in), out_buf(out){}
+    void switch_pair()
+    {
+      auto* tmp = out_buf;
+      out_buf = in_buf;
+      in_buf = tmp;
+    }
+    buffer_t* in_buf;
+    buffer_t* out_buf;
+  };
 
   //auto& thread_pool = thread_pool_t::instance();
   
@@ -55,54 +68,71 @@ void parallel_merge_sort(Iterator begin, Iterator last)
   buffer_t buffer1(length);
   buffer_t buffer2(length);
   
-  auto buffer_pair = buffer_pair_t(buffer1, buffer2);
+  auto buffer_pair = buffer_pair_t(&buffer1, &buffer2);
+  
+  size_t block_size = std::distance(begin, last) / std::thread::hardware_concurrency();
+  
+  std::vector<range<Iterator>> blocks(std::thread::hardware_concurrency());
+  
+  auto it = begin;
+  
+  for(int i = 0; i < std::thread::hardware_concurrency() ; ++i)
+  {
+    blocks[i] = range<Iterator>(it, it + block_size);
+    it += block_size;
+  }
+  
+  std::vector<std::future<void>> results(blocks.size());
+  size_t i = 0;
+  std::for_each(blocks.begin(), blocks.end(), [&results, &i](auto& range){
+    results[i] = std::async(std::launch::async, [](auto begin, auto last)
+    {
+      std::sort(begin, last);
+    }, range.first, range.last);
+    ++i;
+  });
+  
+  for (auto& res: results)
+    res.wait();
+  
   std::copy(begin, last, buffer1.begin());
   
-  auto switch_buffers = [](const buffer_pair_t& buffer_pair_)
-  {
-    return buffer_pair_t(buffer_pair_.second, buffer_pair_.first);
-  };
-  
-  size_t factor = 2; //length of merged arrays
-  size_t num_step = step_count(length);
+  size_t factor = 1; //length of merged arrays
+  size_t num_step = step_count(blocks.size());
   //size_t merge_num = count_merges(length);
   
   for(int i = 0; i < num_step; ++i)
   {
-    auto& in_buf = buffer_pair.first;
-    auto& out_buf = buffer_pair.second;
+    auto& in_buf = *buffer_pair.in_buf;
+    auto& out_buf = *buffer_pair.out_buf;
     auto out_it = out_buf.data();
     auto left_it = in_buf.data();
     const auto in_buf_end = in_buf.data() + in_buf.size();
-    auto right_it = in_buf.data() + factor/2;
+    auto right_it = in_buf.data() + block_size * factor;
+    
+    std::list<std::future<void>> results;
     
     for(; left_it < in_buf_end ;)
     {
-     //   do
-     //   {
-//          auto res = thread_pool.exec([=]()
-//                                      {
-                                        merge(std::min(left_it, in_buf_end),  std::min((left_it + factor/2), in_buf_end ),
-                                              std::min(right_it, in_buf_end), std::min((right_it + factor/2), in_buf_end ),
-                                              out_it);
-  //                                    });
-     //     if(!res)
-     //     {
-      //      thread_pool.restart();
-     //       continue;
-     //     }
-     //     break;
-     //   } while (true);
-      left_it += factor;
-      right_it += factor;
-      out_it += factor;
+      results.push_back(std::async(std::launch::async, [=]()
+      {
+        merge(std::min(left_it, in_buf_end),  std::min((left_it + block_size * factor), in_buf_end ),
+              std::min(right_it, in_buf_end), std::min((right_it + block_size * factor), in_buf_end ),
+              out_it);
+      }));
+      left_it += block_size * factor * 2;
+      right_it += block_size * factor * 2;
+      out_it += block_size * factor * 2;
     }
+    for (auto& res: results)
+      res.wait();
     //thread_pool.restart();
-    buffer_pair = switch_buffers(buffer_pair);
+    buffer_pair.switch_pair();
     factor *= 2;
     //merge_num = count_merges(merge_num);
   }
-  auto& result_buf = buffer_pair.first;
+  buffer_pair.switch_pair();
+  auto& result_buf = *buffer_pair.out_buf;
   std::copy(result_buf.begin(), result_buf.end(), begin);
 }
 
