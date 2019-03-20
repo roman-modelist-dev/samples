5fd3dbe744f74966a309e2af3beed647e9b86354
roman-modelist-dev <ibr.roman@gmail.com>
2019-03-20 11:02:19 +0000

    add json serializer

--- test_sort2/main.cpp
+++ test_sort2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <vector>
 #include <list>
@@ -12,81 +13,63 @@
 #include "json.hpp"
 
 int main() {
-  std::vector<std::string> in_data1 = {"gfhkgh", "77567bgfdh","","ljgxhzgfg","asdfqw1234325", "af3t t9unj9-wn=0vmerwg", "12213", "dfjhgkr", "mhioertphnj SGSdgadg"};
-  std::vector<std::string> in_data2 = {"gfhkgh", "77567bgfdh","","ljgxhzgfg","asdfqw1234325", "af3t t9unj9-wn=0vmerwg", "12213", "dfjhgkr", "mhioertphnj SGSdgadg"};
-  std::vector<std::string> in_data3 = {"gfhkgh", "77567bgfdh","","ljgxhzgfg","asdfqw1234325", "af3t t9unj9-wn=0vmerwg", "12213", "dfjhgkr", "mhioertphnj SGSdgadg"};
-  
-  
-  /*std::vector<int> in_data1(0x0fffffff);
-  for(int i = 0; i < in_data1.size(); ++i)
+  std::ifstream fin("test_string_data.json");
+  nlohmann::json in_data;
+  fin >> in_data;
+  using string_data_set = std::vector<std::string>;
+  std::vector<string_data_set> in_sets;
+  assert(in_data.is_array());
+  for(auto& set: in_data)
   {
-    in_data1[i] = dist(rd);
-  }*/
-  
-//std::vector<int> in_data1(0x0fffffff);
-/*  std::vector<int> in_data1(0x04ffffff);
-  std::vector<int> in_data2(0x04ffffff);
-  std::vector<int> in_data3(0x04ffffff);
-  
-  auto block_size = in_data1.size()/8;
-  size_t current_ind = 0;
-  std::vector<std::future<void>> results;
-  std::random_device rd;
-  std::uniform_int_distribution<uint64_t> dist(0, (uint32_t)-1);
-  
-  for(int i = 0; i < in_data1.size(); ++i)
-  {
-    in_data1[i] = dist(rd);
-  }
-   
-  std::copy(in_data1.begin(), in_data1.end(), in_data2.begin());
-  std::copy(in_data1.begin(), in_data1.end(), in_data3.begin());*/
- /*
-  std::vector<int> in_data2 = {
-   1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32
-  };
- */
- // auto& thread_pool = thread_pool_t::instance();
-  
-  {
-    auto start = std::chrono::system_clock::now();
-    //parallel_merge_sort(in_data1.begin(), in_data1.end());
-    std::sort(in_data1.begin(), in_data1.end());
-    //boost::sort::block_indirect_sort(in_data1.begin(), in_data1.end());
-    auto end = std::chrono::system_clock::now();
-  
-    std::chrono::milliseconds diff = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
-    std::cout << "STD Sort time = " << diff.count() << " ms\n";
+    in_sets.push_back(set.get<string_data_set>());
   }
   
+  for(auto& in_set: in_sets)
   {
-    auto start = std::chrono::system_clock::now();
-    parallel_merge_sort(in_data2.begin(), in_data2.end());
-    //boost::sort::block_indirect_sort(in_data1.begin(), in_data1.end());
-    auto end = std::chrono::system_clock::now();
+    string_data_set copy2(in_set.size()), copy3(in_set.size());
+    std::copy(in_set.begin(), in_set.end(), copy2.begin());
+    std::copy(in_set.begin(), in_set.end(), copy3.begin());
     
-    std::chrono::milliseconds diff = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
-    std::cout << "Parallel Sort time = " << diff.count() << " ms\n";
-  }
-  {
-    auto start = std::chrono::system_clock::now();
-    boost::sort::block_indirect_sort(in_data3.begin(), in_data3.end());
-    auto end = std::chrono::system_clock::now();
+    {
+      auto start = std::chrono::system_clock::now();
+      parallel_merge_sort(in_set.begin(), in_set.end());
+      //boost::sort::block_indirect_sort(in_data1.begin(), in_data1.end());
+      auto end = std::chrono::system_clock::now();
     
-    std::chrono::milliseconds diff = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
-    std::cout << "Boost Sort time = " << diff.count() << " ms\n";
+      std::chrono::milliseconds diff = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
+      std::cout << "parallel sort time = " << diff.count() << " ms\n";
+    }
+    {
+      auto start = std::chrono::system_clock::now();
+      //parallel_merge_sort(in_data1.begin(), in_data1.end());
+      boost::sort::pdqsort(copy2.begin(), copy2.end());
+      //boost::sort::block_indirect_sort(in_data1.begin(), in_data1.end());
+      auto end = std::chrono::system_clock::now();
+    
+      std::chrono::milliseconds diff = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
+      std::cout << "std sort time = " << diff.count() << " ms\n";
+    }
+    {
+      auto start = std::chrono::system_clock::now();
+      boost::sort::block_indirect_sort(copy3.begin(), copy3.end());
+      auto end = std::chrono::system_clock::now();
+    
+      std::chrono::milliseconds diff = std::chrono::duration_cast<std::chrono::milliseconds> (end - start);
+      std::cout << "boost Sort time = " << diff.count() << " ms\n";
+    }
+    bool res = std::equal(in_set.begin(), in_set.end(), copy2.begin());
+    if(res)
+      std::cout << "All OK" << std::endl;
+    else
+      std::cout << "FAIL" << std::endl;
   }
+  std::ofstream fout("string_data_out.json");
+  nlohmann::json j_out;
+  for(auto& in_set: in_sets)
+  {
+    j_out.push_back(in_set);
+  }
+  fout << j_out;
   
-  //boost::sort::block_indirect_sort(in_data1.begin(), in_data1.end());
-  //string_list in_data = {5,23,39,6,14,13,1,6,7,4,8,5,8,9,3,5,23};
-  //string_list in_data = {};
-  //string_list in_data = {5,23,39,6,14,13,1,6,7,4,8,5,8,9,3,5,23};
-  //string_list in_data = {30,23,39};
-  
-  bool res = std::equal(in_data1.begin(), in_data1.end(), in_data2.begin());
-  if(res)
-    std::cout << "All OK" << std::endl;
-  else
-    std::cout << "FAIL" << std::endl;
   return 0;
 }
